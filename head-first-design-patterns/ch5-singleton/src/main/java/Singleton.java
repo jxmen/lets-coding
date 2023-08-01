04e3d91372ce52ea32dd5c4a7dc7f7a018a686a5
pangnem <jxmen@wemeetmobility.com>
2023-08-01 23:13:08 +0900

    DCL(Double-Checked Locking) 추가한 싱글턴으로 변경
    See Also:
    - https://nesoy.github.io/articles/2018-06/Java-volatile

--- head-first-design-patterns/ch5-singleton/src/main/java/Singleton.java
+++ head-first-design-patterns/ch5-singleton/src/main/java/Singleton.java
@@ -1,12 +1,24 @@
 public class Singleton {
-	private static Singleton instance;
+	/**
+	 * Note: volatile은 멀티스레드 환경에서 CPU Cache가 아닌 Main Memory에서 값을 읽어들이겠다고 선언하는 것이다.
+	 * 하나의 스레드가 write하고 나머지 스레드가 read하는 상황에서 쓰면 적합하다.
+	 * 다만, 동시에 여러 스레드가 write하는 상황에서는 데이터의 원자성을 보장할 수 없다.
+	 * <p>
+	 * {@link} https://nesoy.github.io/articles/2018-06/Java-volatile
+	 */
+	private volatile static Singleton instance;
 
 	private Singleton() {
 	}
 
-	public static Singleton getInstance() {
+	public static synchronized Singleton getInstance() {
 		if (instance == null) {
-			instance = new Singleton();
+			synchronized (Singleton.class) {
+				// DCL (Double-Checked Locking)
+				if (instance == null) {
+					instance = new Singleton();
+				}
+			}
 		}
 
 		return instance;
