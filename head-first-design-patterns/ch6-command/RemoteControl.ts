415d29bcff4a725c886dd0c0d7e770488b98066f
pangnem <jxmen@wemeetmobility.com>
2023-08-07 21:28:24 +0900

    커맨드 패턴 취소 동작 구현
    - command 인터페이스에 undo() 메서드 추가
    - 이전에 실행한 커맨드 상태를 필드로 저장 후, undo() 메서드 실행

--- head-first-design-patterns/ch6-command/RemoteControl.ts
+++ head-first-design-patterns/ch6-command/RemoteControl.ts
@@ -1,15 +1,21 @@
 interface Command {
     execute(): void;
+
+    undo(): void;
 }
 
 class NoCommand implements Command {
     execute(): void {
     }
+
+    undo(): void {
+    }
 }
 
 class RemoteControl {
-    private onCommands :Command[];
-    private offCommands :Command[];
+    private onCommands: Command[];
+    private offCommands: Command[];
+    private undoCommand: Command;
 
     constructor() {
         this.onCommands = new Array(7);
@@ -18,6 +24,7 @@ class RemoteControl {
         const noCommand: Command = new NoCommand();
         this.onCommands.forEach(command => command = noCommand);
         this.offCommands.forEach(command => command = noCommand);
+        this.undoCommand = noCommand;
     }
 
     public setCommand(slot: number, onCommand: Command, offCommand: Command) {
@@ -25,11 +32,74 @@ class RemoteControl {
         this.offCommands[slot] = offCommand;
     }
 
-    public onCommandWasPushed(slot: number) {
+    public onButtonWasPushed(slot: number) {
         this.onCommands[slot].execute();
+        this.undoCommand = this.onCommands[slot];
     }
 
-    public offCommandWasPushed(slot: number) {
+    public offButtonWasPushed(slot: number) {
         this.offCommands[slot].execute();
+        this.undoCommand = this.offCommands[slot];
+    }
+
+    public undoButtonWasPushed() {
+        this.undoCommand.undo();
     }
 }
+
+class OnCommand implements Command {
+    private light: Light;
+
+    constructor(light: Light) {
+        this.light = light;
+    }
+
+    execute(): void {
+        this.light.on();
+    }
+
+    undo(): void {
+        this.light.off();
+    }
+}
+
+class Light {
+    off() {
+        console.log("조명이 꺼졌습니다.");
+    }
+
+    on() {
+        console.log("조명이 켜졌습니다.");
+    }
+}
+
+class OffCommand implements Command {
+    private light: Light;
+
+    constructor(light: Light) {
+        this.light = light;
+    }
+
+    execute(): void {
+        this.light.off();
+    }
+
+    undo(): void {
+        this.light.on();
+    }
+}
+
+function main() {
+    const remoteControl = new RemoteControl();
+
+    const light: Light = new Light();
+    const onCommand: Command = new OnCommand(light);
+    const offCommand: Command = new OffCommand(light);
+
+    remoteControl.setCommand(0, onCommand, offCommand);
+    remoteControl.onButtonWasPushed(0);
+    remoteControl.undoButtonWasPushed();
+    remoteControl.offButtonWasPushed(0);
+}
+
+main();
