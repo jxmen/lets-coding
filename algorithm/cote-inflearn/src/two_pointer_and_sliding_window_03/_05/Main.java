eeda60768f81bf0412142d6f99e06c5c1ffff5ac
jxmen <sprnd645@gmail.com>
2023-10-19 00:35:25 +0900

    연속된 자연수의 합 효율적으로 구현한 코드 추가

--- algorithm/cote-inflearn/src/two_pointer_and_sliding_window_03/_05/Main.java
+++ algorithm/cote-inflearn/src/two_pointer_and_sliding_window_03/_05/Main.java
@@ -11,9 +11,39 @@ public class Main {
 	}
 
 	/**
-	 * TODO: 이 방법으로 풀어도 괜찮을지...
+	 * 최적화한 새로운 답
 	 */
 	private int solution(int n) {
+		int answer = 0, sum = 0, lt = 0;
+
+		int m = (n / 2) + 1;
+
+		int[] arr = new int[m];
+		for (int i = 0; i < m; i++) {
+			arr[i] = i + 1;
+		}
+
+		for (int rt = 0; rt < m; rt++) {
+			sum += arr[rt];
+			if (sum == n) {
+				answer++;
+			}
+
+			while (sum >= n) {
+				sum -= arr[lt++];
+				if (sum == n) {
+					answer++;
+				}
+			}
+		}
+
+		return answer;
+	}
+
+	/**
+	 * 처음에 푼 방식. 1~n까지 탐색해야 해서 비효율적
+	 */
+	private int solution2(int n) {
 		int answer = 0;
 
 		for (int lt = 1; lt < (n / 2) + 1; lt++) {
