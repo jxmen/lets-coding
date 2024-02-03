383e91bb64ad4578c14dbae1b3eaf7edf82c6c4b
jxmen <sprnd645@gmail.com>
2024-02-03 16:07:14 +0900

    list 더하는 케이스 테스트 코드 추가

---
+++ python/test/list/list_add_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class ListTest(unittest.TestCase):
+
+    def test_리스트에서_리스트를_더할경우_원소들이_리스트안으로_들어간다(self):
+        a_list = []
+        b_list = [1, 2, 3]
+
+        a_list += b_list
+
+        self.assertEqual(a_list, [1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
