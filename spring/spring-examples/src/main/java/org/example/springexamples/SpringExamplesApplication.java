845709da37234c897fe303a3f7d00fd1f3cdebd4
jxmen <sprnd645@gmail.com>
2024-02-17 02:15:12 +0900

    spring event async로 추가
    `@Async`를 사용하기 위해서는 `@EnableAsync` 어노테이션을 붙여주어야 한다.
    
    비동기로 동작하기 때문에 `@Order` 어노테이션은 더이상 의미가 없다

--- spring/spring-examples/src/main/java/org/example/springexamples/SpringExamplesApplication.java
+++ spring/spring-examples/src/main/java/org/example/springexamples/SpringExamplesApplication.java
@@ -2,9 +2,16 @@ package org.example.springexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 이걸 붙여야 @Async 어노테이션이 동작한다.
+ *
+ * Thread Pool 관련 설정들도 같이 해주는게 좋다.
+ */
+@EnableAsync
 @RestController
 @SpringBootApplication
 public class SpringExamplesApplication {
--- spring/spring-examples/src/main/java/org/example/springexamples/event/handler/AnotherMyEventHandler.java
+++ spring/spring-examples/src/main/java/org/example/springexamples/event/handler/AnotherMyEventHandler.java
@@ -4,8 +4,7 @@ import org.example.springexamples.event.MyEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,7 @@ public class AnotherMyEventHandler {
 	Logger log = LoggerFactory.getLogger(AnotherMyEventHandler.class);
 
 	@EventListener
-	@Order(Ordered.LOWEST_PRECEDENCE - 1)
+	@Async
 	public void handle(MyEvent event) {
 		log.info("event number is : '{}', thread: '{}'", event.getNumber(), Thread.currentThread().getName());
 	}
