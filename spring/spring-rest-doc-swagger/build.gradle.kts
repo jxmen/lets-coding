795e713b410bef803d8de9e4d427e4c33634fec1
jxmen <sprnd645@gmail.com>
2024-02-28 12:10:51 +0900

    docs: index api swagger 문서 테스트 추가

--- spring/spring-rest-doc-swagger/build.gradle.kts
+++ spring/spring-rest-doc-swagger/build.gradle.kts
@@ -1,7 +1,13 @@
+repositories {
+    mavenCentral()
+}
+
 plugins {
     java
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.epages.restdocs-api-spec") version "0.17.1"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
 group = "org.example"
@@ -29,8 +35,48 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    /**
+     * Spring REST Docs Swagger를 위한 의존성 추가
+     *
+     * https://mvnrepository.com/artifact/com.epages/restdocs-api-spec-mockmvc
+     */
+    implementation("com.epages:restdocs-api-spec:0.17.1")
+    implementation("com.epages:restdocs-api-spec-mockmvc:0.17.1")
+    implementation("com.epages:restdocs-api-spec-openapi3-generator:0.17.1")
+
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val snippetsDir by extra { file("build/generated-snippets") }
+
+tasks.test {
+    outputs.dir(snippetsDir)
+
+    finalizedBy("openapi3")
+}
+
+tasks.asciidoctor {
+    inputs.dir(snippetsDir)
+    dependsOn(tasks.test)
+}
+
+tasks.bootJar {
+    archiveFileName.set("app.jar")
+
+    dependsOn(tasks.test)
+
+    from("${tasks.asciidoctor.get().outputDir}") {
+        into("BOOT-INF/classes/static/docs")
+    }
+    from("swagger-ui") {
+        into("BOOT-INF/classes/static/swagger")
+    }
+    from("build/api-spec") {
+        into("BOOT-INF/classes/static/swagger")
+    }
 }
--- spring/spring-rest-doc-swagger/src/test/java/org/example/swagger/ApplicationTests.java
+++ spring/spring-rest-doc-swagger/src/test/java/org/example/swagger/ApplicationTests.java
@@ -1,18 +1,24 @@
 package org.example.swagger;
 
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 class ApplicationTests {
 	private final MockMvc mockMvc;
 
@@ -22,9 +28,16 @@ class ApplicationTests {
 
 	@Test
 	void contextLoads() throws Exception {
-		mockMvc.perform(get("/"))
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/"))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.message").value("hello"));
+				.andExpect(jsonPath("$.message").value("hello"))
+				.andDo(
+						MockMvcRestDocumentationWrapper.document("get-advertisement-transmission",
+								responseFields(
+										fieldWithPath("message").description("it must be hello")
+								)
+						)
+				);
 	}
 
 }
