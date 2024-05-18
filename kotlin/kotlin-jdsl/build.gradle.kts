08cf1ebc572f6d45cc7d37d20bee8b802ffb208b
jxmen <sprnd645@gmail.com>
2024-05-18 15:07:48 +0900

    kotlin jdsl 관련 의존성 추가
    https://kotlin-jdsl.gitbook.io/docs/v/ko-1/jpql-with-kotlin-jdsl

--- kotlin/kotlin-jdsl/build.gradle.kts
+++ kotlin/kotlin-jdsl/build.gradle.kts
@@ -17,6 +17,7 @@ java {
 
 repositories {
     mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots") // kotlin jdsl oss snapshot repository
 }
 
 dependencies {
@@ -24,6 +25,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-devtools") // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-devtools
+
+    // kotlin jdsl dependencies
+    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.4.1") // JPQL을 만들어 주도록 도와주는 라이브러리
+    implementation("com.linecorp.kotlin-jdsl:jpql-render:3.4.1") // DSL로 만든 쿼리를 String으로 변환해주는 라이브러리
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
