55a8b8c21863b94380551432bb746b8176f60d28
jxmen <sprnd645@gmail.com>
2024-09-03 16:50:13 +0900

    JPA 영속성 컨텍스트 clear 예제 추가

--- java/jpa-test/src/test/java/org/example/entity/MemberTest.java
+++ java/jpa-test/src/test/java/org/example/entity/MemberTest.java
@@ -1,5 +1,7 @@
 package org.example.entity;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
@@ -9,19 +11,31 @@ import javax.persistence.Persistence;
 
 class MemberTest {
 
+	private EntityManagerFactory emf;
+	private EntityManager em;
+
+	@BeforeEach
+	void setUp() {
+		emf = Persistence.createEntityManagerFactory("example-unit");
+		em = emf.createEntityManager();
+	}
+
+	@AfterEach
+	void tearDown() {
+		em.close();
+		emf.close();
+	}
+
 	@Test
 	void persistence_context_test() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("example-unit");
-		EntityManager em = emf.createEntityManager();
-
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 
-		try {
-			// 비영속
-			Member member = new Member(1L, "memberA", 10);
-			Member member2 = new Member(2L, "memberB", 20);
+		// 비영속
+		Member member = new Member(1L, "memberA", 10);
+		Member member2 = new Member(2L, "memberB", 20);
 
+		try {
 			System.out.println("===== persist member =====");
 			// 영속 (쿼리가 바로 나가지 않음)
 			em.persist(member);
@@ -38,10 +52,38 @@ class MemberTest {
 			em.find(Member.class, 100L);
 		} catch (Exception e) {
 			tx.rollback();
-		} finally {
-			em.close();
+		}
+	}
+
+	@Test
+	void 영속성_컨텍스트를_clear하면_sql을_실행한다() {
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		// 비영속
+		Member member = new Member(1L, "memberA", 10);
+		Member member2 = new Member(2L, "memberB", 20);
+
+		try {
+			System.out.println("===== persist member =====");
+			// 영속 (쿼리가 바로 나가지 않음)
+			em.persist(member);
+			em.persist(member2);
+
+			System.out.println("===== before commit =====");
+			// 영속성 컨텍스트에 있는 데이터를 DB에 반영, 쿼리 실행
+			tx.commit();
+			System.out.println("===== after commit =====");
+
+		} catch (Exception e) {
+			tx.rollback();
 		}
 
-		emf.close();
+		em.clear();
+
+		// clear했기 때문에 모두 sql에서 가져온다.
+		em.find(Member.class, member.getId());
+		em.find(Member.class, member2.getId());
+		em.find(Member.class, 100L);
 	}
 }
