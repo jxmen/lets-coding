363556507836cb6511743ab78a27e3c437540be5
jxmen <sprnd645@gmail.com>
2024-09-26 15:24:52 +0900

    재고 시스템 강의 프로젝트 세팅

---
+++ lectures/inflearn/stock-concurrency-project/src/test/java/org/example/stock/StockConcurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockConcurrencyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
